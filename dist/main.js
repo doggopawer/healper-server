94742e0d8c598d6b69885d597632dd655ffb26da
doggopawer <doggopawer@gmail.com>
2024-03-19 02:25:14 +0900

    feat: DB 연동

--- dist/main.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log("타입스크립트");
-//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("타입스크립트");
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("healper", "root", "xxxx", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("연결 성공"))
+  .catch(() => console.log("연결 실패"));
