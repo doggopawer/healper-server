823c4ca78e40a7bbec66345b6e4b31be79e15acb
doggopawer <doggopawer@gmail.com>
2024-03-20 02:02:01 +0900

    feat: oauth 로그인 기본 설정 완료

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,15 @@ type Config = {
     password: string | undefined;
     database: string | undefined;
   };
+  oauth: {
+    google: {
+      clientId: string | undefined;
+      clientSecret: string | undefined;
+      redirectUrl: string | undefined;
+      tokenUrl: string | undefined;
+      userInfoUrl: string | undefined;
+    };
+  };
 };
 
 export const config: Config = {
@@ -15,5 +24,14 @@ export const config: Config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+  },
+  oauth: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      redirectUrl: process.env.GOOGLE_REDIRECT_URL,
+      tokenUrl: process.env.GOOGLE_TOKEN_URL,
+      userInfoUrl: process.env.GOOGLE_USERINFO_URL,
+    },
   },
 };
