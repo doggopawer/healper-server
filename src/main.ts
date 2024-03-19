0a613bfac7968f5e728f82854da31545f8db4b25
doggopawer <doggopawer@gmail.com>
2024-03-20 03:27:44 +0900

    feat: 각 도메인 별 라우터 생성

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,14 @@ import cors from "cors";
 import morgan from "morgan";
 import axios from "axios";
 import { config } from "./config";
+import UserRouter from "./routes/User";
+import WorkoutLibraryRouter from "./routes/workout-library";
+import RoutineConfigRouter from "./routes/routine-config";
+import WorkoutConfigRouter from "./routes/workout-config";
+import SetConfigRouter from "./routes/set-config";
+import RoutineRecordRouter from "./routes/routine-record";
+import WorkoutRecordRouter from "./routes/workout-record";
+import SetRecordRouter from "./routes/set-record";
 
 User;
 RoutineConfig;
@@ -29,6 +37,15 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
+
+app.use("/user", UserRouter);
+app.use("/workout-library", WorkoutLibraryRouter);
+app.use("/routine-config", RoutineConfigRouter);
+app.use("/workout-config", WorkoutConfigRouter);
+app.use("/set-config", SetConfigRouter);
+app.use("/routine-record", RoutineRecordRouter);
+app.use("/workout-record", WorkoutRecordRouter);
+app.use("/set-record", SetRecordRouter);
 
 const { clientId, clientSecret, redirectUrl, tokenUrl, userInfoUrl } =
   config.oauth.google;
