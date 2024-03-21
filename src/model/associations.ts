4f48004dd81acef23d1ac255d53558aa73409101
doggopawer <doggopawer@gmail.com>
2024-03-22 00:17:49 +0900

    fix: hasMany, belongsTo 관계 설정 수정

--- src/model/associations.ts
+++ src/model/associations.ts
@@ -7,26 +7,50 @@ import { WorkoutConfig } from "./workout-config";
 import { WorkoutLibrary } from "./workout-library";
 import { WorkoutRecord } from "./workout-record";
 
-User.hasMany(RoutineConfig);
-RoutineConfig.belongsTo(User);
+User.hasMany(RoutineConfig, { as: "routineConfigs", foreignKey: "userId" });
+RoutineConfig.belongsTo(User, { as: "user", foreignKey: "userId" });
 
-User.hasMany(RoutineRecord);
-RoutineRecord.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(RoutineRecord, { as: "routineRecords", foreignKey: "userId" });
+RoutineRecord.belongsTo(User, { as: "user", foreignKey: "userId" });
 
-User.hasMany(WorkoutRecord);
-WorkoutRecord.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(WorkoutRecord, { as: "workoutRecords", foreignKey: "userId" });
+WorkoutRecord.belongsTo(User, { as: "user", foreignKey: "userId" });
 
-User.hasMany(WorkoutLibrary);
-WorkoutLibrary.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(WorkoutLibrary, { as: "workoutLibraries", foreignKey: "userId" });
+WorkoutLibrary.belongsTo(User, { as: "user", foreignKey: "userId" });
 
-RoutineConfig.hasMany(WorkoutConfig);
-WorkoutConfig.belongsTo(RoutineConfig);
+RoutineConfig.hasMany(WorkoutConfig, {
+  as: "workoutConfigs",
+  foreignKey: "routineConfigId",
+});
+WorkoutConfig.belongsTo(RoutineConfig, {
+  as: "routineConfig",
+  foreignKey: "routineConfigId",
+});
 
-WorkoutConfig.hasMany(SetConfig);
-SetConfig.belongsTo(WorkoutConfig, { foreignKey: "workoutConfigId" });
+WorkoutConfig.hasMany(SetConfig, {
+  as: "setConfigs",
+  foreignKey: "workoutConfigId",
+});
+SetConfig.belongsTo(WorkoutConfig, {
+  as: "workoutConfig",
+  foreignKey: "workoutConfigId",
+});
 
-RoutineRecord.hasMany(WorkoutRecord);
-WorkoutRecord.belongsTo(RoutineRecord, { foreignKey: "routineRecordId" });
+RoutineRecord.hasMany(WorkoutRecord, {
+  as: "workoutRecords",
+  foreignKey: "routineRecordId",
+});
+WorkoutRecord.belongsTo(RoutineRecord, {
+  as: "routineRecord",
+  foreignKey: "routineRecordId",
+});
 
-WorkoutRecord.hasMany(SetRecord);
-SetRecord.belongsTo(WorkoutRecord, { foreignKey: "workoutRecordId" });
+WorkoutRecord.hasMany(SetRecord, {
+  as: "setRecords",
+  foreignKey: "workoutRecordId",
+});
+SetRecord.belongsTo(WorkoutRecord, {
+  as: "workoutRecord",
+  foreignKey: "workoutRecordId",
+});
