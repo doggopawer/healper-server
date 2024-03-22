9be2e2deb7d82cd42ff3afbf9a29a7185c6b1065
doggopawer <doggopawer@gmail.com>
2024-03-22 22:02:13 +0900

    feat: UserController, loginRedirectUser 로직 추가 - OAuth의 정보를 받아 JWT 토큰 발급

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,32 +1,25 @@
 import { Request, Response } from "express";
 import { config } from "../config";
 import axios from "axios";
+import * as userRepository from "../model/user";
+import { createJwtToken } from "../util";
 
-const { clientId, clientSecret, redirectUrl, tokenUrl, userInfoUrl } =
-  config.oauth.google;
 export const loginUser = async (req: Request, res: Response) => {
+  const { clientId, redirectUrl } = config.oauth.google;
   let url = "https://accounts.google.com/o/oauth2/v2/auth";
-  // client_id는 위 스크린샷을 보면 발급 받았음을 알 수 있음
-  // 단, 스크린샷에 있는 ID가 아닌 당신이 직접 발급 받은 ID를 사용해야 함.
   url += `?client_id=${clientId}`;
-  // 아까 등록한 redirect_uri
-  // 로그인 창에서 계정을 선택하면 구글 서버가 이 redirect_uri로 redirect 시켜줌
   url += `&redirect_uri=${redirectUrl}`;
-  // 필수 옵션.
   url += "&response_type=code";
-  // 구글에 등록된 유저 정보 email, profile을 가져오겠다 명시
   url += "&scope=email profile";
-  // 완성된 url로 이동
-  // 이 url이 위에서 본 구글 계정을 선택하는 화면임.
   res.redirect(url);
 };
 
 export const loginRedirectUser = async (req: Request, res: Response) => {
+  const { clientId, clientSecret, redirectUrl, tokenUrl, userInfoUrl } =
+    config.oauth.google;
   const { code } = req.query;
-  console.log(`code: ${code}`);
 
-  const resp = await axios.post(tokenUrl as string, {
-    // x-www-form-urlencoded(body)
+  const tokenUrlResponse = await axios.post(tokenUrl as string, {
     code,
     client_id: clientId,
     client_secret: clientSecret,
@@ -34,13 +27,27 @@ export const loginRedirectUser = async (req: Request, res: Response) => {
     grant_type: "authorization_code",
   });
 
-  const resp2 = await axios.get(userInfoUrl as string, {
-    // Request Header에 Authorization 추가
+  const userInfoUrlResponse = await axios.get(userInfoUrl as string, {
     headers: {
-      Authorization: `Bearer ${resp.data.access_token}`,
+      Authorization: `Bearer ${tokenUrlResponse.data.access_token}`,
     },
   });
 
-  // 구글 인증 서버에서 json 형태로 반환 받은 body 클라이언트에 반환
-  res.json(resp2.data);
+  const { id, email, name, picture } = userInfoUrlResponse.data;
+  const found = await userRepository.getOneById(id);
+
+  if (!found) {
+    await userRepository.createOne({
+      id,
+      email,
+      name,
+      picture,
+    });
+  }
+
+  //TODO: 유저의 정보를 업데이트 해주어 소셜네트워크와 동기화 시켜준다.
+
+  const token = createJwtToken(id);
+
+  res.status(201).json({ token, id });
 };
