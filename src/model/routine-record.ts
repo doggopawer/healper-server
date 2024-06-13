4a2cc227d73da5bda423d59a2c1bbb83f14652b3
dhkim <dhkim@i2l.co.kr>
2024-06-13 18:23:50 +0900

    feat: RoutineRecord, createOne 제작

--- src/model/routine-record.ts
+++ src/model/routine-record.ts
@@ -2,43 +2,59 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../db";
 
 export const RoutineRecord = sequelize.define("RoutineRecord", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  color: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    color: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
 });
 
 export async function getAll(userId: number) {
-  try {
-    return await RoutineRecord.findAll({
-      where: {
-        userId,
-      },
-    });
-  } catch (err) {
-    throw new Error(err as string); // err가 Error 타입임을 가정
-  }
+    try {
+        return await RoutineRecord.findAll({
+            where: {
+                userId,
+            },
+        });
+    } catch (err) {
+        throw new Error(err as string); // err가 Error 타입임을 가정
+    }
 }
 
 export async function getOneById(userId: number, id: number) {
-  try {
-    return await RoutineRecord.findOne({
-      where: {
-        userId, // 해당유저의
-        id, // routineId값이 일치하는 routine을 가져온다.
-      },
-    });
-  } catch (err) {
-    throw new Error(err as string); // err가 Error 타입임을 가정
-  }
+    try {
+        return await RoutineRecord.findOne({
+            where: {
+                userId, // 해당유저의
+                id, // routineId값이 일치하는 routine을 가져온다.
+            },
+        });
+    } catch (err) {
+        throw new Error(err as string); // err가 Error 타입임을 가정
+    }
+}
+
+type CreateRoutineRecordRequest = {
+    name: string;
+    color: string;
+    userId: number;
+};
+export async function createOne(
+    CreateRoutineConfigRequest: CreateRoutineRecordRequest
+) {
+    try {
+        const data = await RoutineRecord.create(CreateRoutineConfigRequest);
+        return data.dataValues;
+    } catch (err) {
+        throw new Error(err as string);
+    }
 }
