33f8fbcf1260e2751cf98ff1b70477fec4dc4f5e
dhkim <dhkim@i2l.co.kr>
2024-06-23 17:51:17 +0900

    feat: RoutineRecordRouter 수정 - 컨트롤러 네이밍 수정

--- src/routes/routine-record.ts
+++ src/routes/routine-record.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import {
+    createRoutineRecordOne,
+    deleteRoutineRecordOne,
+    getRoutineRecordAll,
+    getRoutineRecordOne,
+    updateRoutineRecordOne,
+} from "../controller/routine-record";
+import { isAuth } from "../middleware";
 
 const router = express.Router();
 
-router.get("/");
-router.get("/:id");
-router.post("/");
-router.put("/:id");
-router.delete("/:id");
+router.get("/", isAuth, getRoutineRecordAll);
+router.get("/:id", isAuth, getRoutineRecordOne);
+router.post("/", isAuth, createRoutineRecordOne);
+router.put("/:id", isAuth, updateRoutineRecordOne);
+router.delete("/:id", isAuth, deleteRoutineRecordOne);
 
 export default router;
