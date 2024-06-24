c44a64f51a1cdf68f5117bd5effecf938d053997
dhkim <dhkim@i2l.co.kr>
2024-06-24 18:14:38 +0900

    feat: WorkoutRecord, getAll 함수 생성

--- src/model/workout-record.ts
+++ src/model/workout-record.ts
@@ -2,22 +2,34 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../db";
 
 export const WorkoutRecord = sequelize.define("WorkoutRecord", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  workoutImage: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  type: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    workoutImage: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    type: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
 });
+
+export async function getAll(routineRecordId: number) {
+    try {
+        return await WorkoutRecord.findAll({
+            where: {
+                routineRecordId,
+            },
+        });
+    } catch (err) {
+        throw new Error(err as string); // err가 Error 타입임을 가정
+    }
+}
