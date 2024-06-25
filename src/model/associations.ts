ab12454210b29ae3e19c06fdb0589d9d819ce259
dhkim <dhkim@i2l.co.kr>
2024-06-25 12:33:16 +0900

    feat: associations.ts 관계 수정 => User.hasMany(WorkoutRecord) 주석 처리 => WorkoutRecord.belongsTo(User) 주석 처리

--- src/model/associations.ts
+++ src/model/associations.ts
@@ -8,69 +8,69 @@ import { WorkoutLibrary } from "./workout-library";
 import { WorkoutRecord } from "./workout-record";
 
 User.hasMany(RoutineConfig, {
-  as: "routineConfigs",
-  foreignKey: "userId",
-  onDelete: "CASCADE",
+    as: "routineConfigs",
+    foreignKey: "userId",
+    onDelete: "CASCADE",
 });
 RoutineConfig.belongsTo(User, { as: "user", foreignKey: "userId" });
 
 User.hasMany(RoutineRecord, {
-  as: "routineRecords",
-  foreignKey: "userId",
-  onDelete: "CASCADE",
+    as: "routineRecords",
+    foreignKey: "userId",
+    onDelete: "CASCADE",
 });
 RoutineRecord.belongsTo(User, { as: "user", foreignKey: "userId" });
 
-User.hasMany(WorkoutRecord, {
-  as: "workoutRecords",
-  foreignKey: "userId",
-  onDelete: "CASCADE",
-});
-WorkoutRecord.belongsTo(User, { as: "user", foreignKey: "userId" });
+// User.hasMany(WorkoutRecord, {
+//     as: "workoutRecords",
+//     foreignKey: "userId",
+//     onDelete: "CASCADE",
+// });
+// WorkoutRecord.belongsTo(User, { as: "user", foreignKey: "userId" });
 
 User.hasMany(WorkoutLibrary, {
-  as: "workoutLibraries",
-  foreignKey: "userId",
-  onDelete: "CASCADE",
+    as: "workoutLibraries",
+    foreignKey: "userId",
+    onDelete: "CASCADE",
 });
 WorkoutLibrary.belongsTo(User, { as: "user", foreignKey: "userId" });
 
 RoutineConfig.hasMany(WorkoutConfig, {
-  as: "workoutConfigs",
-  foreignKey: "routineConfigId",
-  onDelete: "CASCADE",
+    as: "workoutConfigs",
+    foreignKey: "routineConfigId",
+    onDelete: "CASCADE",
 });
 WorkoutConfig.belongsTo(RoutineConfig, {
-  as: "routineConfig",
-  foreignKey: "routineConfigId",
+    as: "routineConfig",
+    foreignKey: "routineConfigId",
 });
 
 WorkoutConfig.hasMany(SetConfig, {
-  as: "setConfigs",
-  foreignKey: "workoutConfigId",
-  onDelete: "CASCADE",
+    as: "setConfigs",
+    foreignKey: "workoutConfigId",
+    onDelete: "CASCADE",
 });
 SetConfig.belongsTo(WorkoutConfig, {
-  as: "workoutConfig",
-  foreignKey: "workoutConfigId",
+    as: "workoutConfig",
+    foreignKey: "workoutConfigId",
 });
 
 RoutineRecord.hasMany(WorkoutRecord, {
-  as: "workoutRecords",
-  foreignKey: "routineRecordId",
-  onDelete: "CASCADE",
+    as: "workoutRecords",
+    foreignKey: "routineRecordId",
+    onDelete: "CASCADE",
 });
 WorkoutRecord.belongsTo(RoutineRecord, {
-  as: "routineRecord",
-  foreignKey: "routineRecordId",
+    as: "routineRecord",
+    foreignKey: "routineRecordId",
 });
 
 WorkoutRecord.hasMany(SetRecord, {
-  as: "setRecords",
-  foreignKey: "workoutRecordId",
-  onDelete: "CASCADE",
+    as: "setRecords",
+    foreignKey: "workoutRecordId",
+    onDelete: "CASCADE",
 });
 SetRecord.belongsTo(WorkoutRecord, {
-  as: "workoutRecord",
-  foreignKey: "workoutRecordId",
+    as: "workoutRecord",
+    foreignKey: "workoutRecordId",
 });
