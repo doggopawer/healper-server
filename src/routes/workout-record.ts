92c32f14ca70fda9f129304e8db0b04b97c7e6d8
dhkim <dhkim@i2l.co.kr>
2024-06-25 12:34:20 +0900

    feat: WorkoutRecordRouter, POST / 추가

--- src/routes/workout-record.ts
+++ src/routes/workout-record.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import { isAuth } from "../middleware";
+import * as WorkoutRecordController from "../controller/workout-record";
 
 const router = express.Router();
 
-router.get("/");
-router.get("/:id");
-router.post("/");
-router.put("/:id");
-router.delete("/:id");
+router.get(
+    "/:routineRecordId",
+    isAuth,
+    WorkoutRecordController.getWorkoutRecordAll
+);
+router.post("/", isAuth, WorkoutRecordController.createWorkoutRecordOne);
 
 export default router;
