215ae562c822b024a4a7d281a5500966fd81180e
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:24:13 +0900

    feat: WorkoutConfigRepsitory 수정 - WorkoutConfigType 적용

--- src/model/workout-config.ts
+++ src/model/workout-config.ts
@@ -1,65 +1,87 @@
-import { DataTypes } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../db";
+import { WorkoutConfig as WorkoutConfigType } from "../types/workout-config";
 
-export const WorkoutConfig = sequelize.define("WorkoutConfig", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  workoutImage: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  type: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+export const WorkoutConfig = sequelize.define<Model<WorkoutConfigType>>(
+    "WorkoutConfig",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        workoutImage: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        type: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        routineConfigId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "routineconfigs",
+                key: "id",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "CASCADE",
+        },
+    }
+);
 
 export async function getAll(routineConfigId: number) {
-  try {
-    return await WorkoutConfig.findAll({
-      where: {
-        routineConfigId,
-      },
-    });
-  } catch (err) {
-    throw new Error(err as string); // err가 Error 타입임을 가정
-  }
+    try {
+        return await WorkoutConfig.findAll({
+            where: {
+                routineConfigId,
+            },
+        });
+    } catch (err) {
+        throw new Error(err as string); // err가 Error 타입임을 가정
+    }
 }
 
 type CreateWorkoutConfigRequest = {
-  name: string;
-  workoutImage: string;
-  type: string;
-  routineConfigId: string;
+    name: string;
+    workoutImage: string;
+    type: string;
+    routineConfigId: string;
 };
-export async function createOne(
-  createWorkoutConfigRequest: CreateWorkoutConfigRequest
-) {
-  try {
-    const data = await WorkoutConfig.create(createWorkoutConfigRequest);
-    return data.dataValues;
-  } catch (err) {
-    throw new Error(err as string);
-  }
+export async function createOne(createWorkoutConfigRequest: any) {
+    try {
+        const data = await WorkoutConfig.create(createWorkoutConfigRequest);
+        return data.dataValues;
+    } catch (err) {
+        throw new Error(err as string);
+    }
 }
 
 export async function deleteOne(id: number) {
-  try {
-    const data = await WorkoutConfig.destroy({
-      where: {
-        id,
-      },
-    });
-    return data;
-  } catch (err) {
-    throw new Error(err as string);
-  }
+    try {
+        const data = await WorkoutConfig.destroy({
+            where: {
+                id,
+            },
+        });
+        return data;
+    } catch (err) {
+        throw new Error(err as string);
+    }
 }
