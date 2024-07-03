84d7e7c1826d6aff303e890cbdbf7cd6632cdede
dhkim <dhkim@i2l.co.kr>
2024-07-03 22:23:56 +0900

    feat: UserRepsitory 수정 - UserType 적용

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,66 +1,67 @@
 import { DataTypes, Model, ModelCtor } from "sequelize";
 import { sequelize } from "../db";
+import { User as UserType } from "../types/user";
 
-export const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  provider: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  providerId: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  profileImage: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
+export const User = sequelize.define<Model<UserType>>("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    provider: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    providerId: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    profileImage: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
 }) as ModelCtor<any>;
 
 export const getOneById = async (providerId: string) => {
-  try {
-    return User.findOne({
-      where: {
-        providerId,
-      },
-    });
-  } catch (err) {
-    throw new Error(err as string);
-  }
+    try {
+        return User.findOne({
+            where: {
+                providerId,
+            },
+        });
+    } catch (err) {
+        throw new Error(err as string);
+    }
 };
 
 type CreateUserRequest = {
-  id: string;
-  email: string;
-  name: string;
-  picture: string;
+    id: string;
+    email: string;
+    name: string;
+    picture: string;
 };
 
 export const createOne = async (createUserRequest: CreateUserRequest) => {
-  const { id, email, name, picture } = createUserRequest;
-  try {
-    const data = await User.create({
-      provider: "Google",
-      providerId: id,
-      name,
-      email,
-      profileImage: picture,
-    });
-    return data.dataValues;
-  } catch (err) {
-    throw new Error(err as string);
-  }
+    const { id, email, name, picture } = createUserRequest;
+    try {
+        const data = await User.create({
+            provider: "Google",
+            providerId: id,
+            name,
+            email,
+            profileImage: picture,
+        });
+        return data.dataValues;
+    } catch (err) {
+        throw new Error(err as string);
+    }
 };
