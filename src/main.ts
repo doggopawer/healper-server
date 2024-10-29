68c1d0f361b37c43118c635ee9e3c1698ff5ed5a
dhkim <dhkim@i2l.co.kr>
2024-10-29 23:25:53 +0900

    remove: 불필요한 파일 일괄 삭제

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
+import * as Controller from "./controller";
+import { isAuth } from "./middleware";
 
 const app = express();
 
@@ -11,20 +13,31 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
 
-// app.use("/user", UserRouter);
-// app.use("/workout-library", WorkoutLibraryRouter);
-// app.use("/routine-config", RoutineConfigRouter);
-// app.use("/workout-config", WorkoutConfigRouter);
-// app.use("/set-config", SetConfigRouter);
-// app.use("/routine-record", RoutineRecordRouter);
-// app.use("/workout-record", WorkoutRecordRouter);
-// app.use("/set-record", SetRecordRouter);
+app.get("/login", Controller.loginUser);
+app.get("/login/redirect", Controller.loginRedirectUser);
+app.get("/sync", isAuth, Controller.syncData);
 
 connectDB()
     .then(() => {
         app.listen(4000, () => {
             console.log("server is running at 4000");
         });
+
+        // const newRoutine = new RoutineConfigModel({
+        //     _id: "1234",
+        //     name: "My Workout Routine",
+        //     color: "blue",
+        //     userId: "user123",
+        // });
+
+        // newRoutine
+        //     .save()
+        //     .then(() => {
+        //         console.log("RoutineConfig Document saved successfully!");
+        //     })
+        //     .catch((err) => {
+        //         console.error("Error saving Document:", err);
+        //     });
     })
     .catch(() => {
         console.error("Unable to connect to the database.");
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,31 +1,35 @@
 import { NextFunction, Request, Response } from "express";
 import { config } from "../config";
 import jwt from "jsonwebtoken";
-import * as userRepository from "../model/user";
+import UserModel from "../model/user";
 
 const AUTH_ERROR = { message: "Authentication Error" };
 
 export const isAuth = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  const authHeader = req.get("Authorization");
-  if (!(authHeader && authHeader.startsWith("Bearer "))) {
-    return res.status(401).json(AUTH_ERROR);
-  }
+    const authHeader = req.get("Authorization");
+    if (!(authHeader && authHeader.startsWith("Bearer "))) {
+        return res.status(401).json(AUTH_ERROR);
+    }
 
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, config.jwt.secretKey as string, async (error, decoded) => {
-    if (error) {
-      return res.status(401).json(AUTH_ERROR);
-    }
-    const payload = decoded as jwt.JwtPayload;
-    const user = await userRepository.getOneById(payload.id);
-    if (!user) {
-      return res.status(401).json(AUTH_ERROR);
-    }
-    res.locals.userId = user.id;
-    next();
-  });
+    const token = authHeader.split(" ")[1];
+    jwt.verify(
+        token,
+        config.jwt.secretKey as string,
+        async (error, decoded) => {
+            if (error) {
+                return res.status(401).json(AUTH_ERROR);
+            }
+            const payload = decoded as jwt.JwtPayload;
+            const user = await UserModel.findById(payload.id);
+            if (!user) {
+                return res.status(401).json(AUTH_ERROR);
+            }
+            res.locals.userId = user.id;
+            next();
+        }
+    );
 };
