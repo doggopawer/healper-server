bce4ffe06c94e44d04bfe0d6ff29a5ced652df39
dhkim <dhkim@i2l.co.kr>
2024-11-07 17:01:47 +0900

    fix: S3 임포트 구문 수정

--- src/s3.ts
+++ src/s3.ts
@@ -1,9 +1,9 @@
 // s3.ts
-import AWS from "aws-sdk";
+import S3 from "aws-sdk/clients/s3";
 import { config } from "./config";
 
 // S3 클라이언트 설정
-const s3 = new AWS.S3({
+const s3 = new S3({
     accessKeyId: config.s3.accessKey, // IAM 사용자에서 받은 Access Key
     secretAccessKey: config.s3.secretAccessKey, // IAM 사용자에서 받은 Secret Access Key
     region: config.s3.region, // S3 버킷의 리전 (예: 'us-east-1')
