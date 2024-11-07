1b8d76e14d180823bd993310551fc7fc24f8ebc0
dhkim <dhkim@i2l.co.kr>
2024-11-07 16:10:51 +0900

    fix: 데이터베이스 커넥팅 오류 콘솔 출력

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 import { config } from "../config";
 const connectDB = () =>
-    mongoose.connect(
-        `mongodb+srv://${config.db.user}:${config.db.password}@healper-db.akgt9.mongodb.net/healper-db`
-    );
+    mongoose
+        .connect(
+            `mongodb+srv://${config.db.user}:${config.db.password}@healper-db.akgt9.mongodb.net/healper-db`
+        )
+        .catch((err) => console.log(err));
 
 export default connectDB;
