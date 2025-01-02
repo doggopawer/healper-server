b6ce56cf3e00fded3ac35b3e0bb07fa623aed8b8
dhkim <dhkim@i2l.co.kr>
2025-01-03 01:03:39 +0900

    feat: URL-encoded 데이터 파싱 추가

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 // middleware 설정
 app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
